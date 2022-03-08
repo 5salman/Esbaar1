afc02e4f8429842ab493bf3e97149aeb04928902
5salman <75927115+5salman@users.noreply.github.com>
2022-03-08 12:47:20 +0400

    Design Test

---
+++ assets/css/js/slider.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+ 
+    $("#owl-demo").owlCarousel({
+      navigation : true,
+      autoplay:true,
+      items : 1, 
+      loop:true,
+      nav:true,
+      
+      animateOut: 'fadeOut',
+         animateIn: 'fadeIn',
+    });
+   
+   });
